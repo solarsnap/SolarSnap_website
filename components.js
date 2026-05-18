198e5c41c951ec1b19fcbae0e6cae20b954daaad
paulwebster44-cmd <paulwebster44@gmail.com>
2026-05-18 11:58:27 +0100

    Add EcoFlow affiliate links, disclosure notices, and recommended products page

--- components.js
+++ components.js
@@ -46,6 +46,7 @@
     <p class="footer-copy">&copy; ${new Date().getFullYear()} SolarSnap. All rights reserved.</p>
     <div class="footer-links">
       <a href="/blog/">Blog</a>
+      <a href="/recommended-products.html">Recommended Products</a>
       <a href="/privacy.html">Privacy Policy</a>
       <a href="/contact.html">Contact</a>
       <a href="mailto:info@solarsnap.co.uk">info@solarsnap.co.uk</a>
